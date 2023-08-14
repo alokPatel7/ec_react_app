e9e3087bf6647f59c4a4b21466f636648f7cb1a8
alok_patel <alokpatel863@gmail.com>
2023-08-14 21:19:43 +0530

    add controls for input fields

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,16 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css';
+import { Button, FormHelperText, IconButton, InputAdornment, TextField } from "@mui/material";
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 export default function LoginComponent() {
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [isEmailValid, setIsEmailValid] = useState(false);
+    const [isPasswordValid, setIsPasswordValid] = useState(false);
+
+    const validateEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+
+    const validatePassword = (password) => {
+        const passwordRegex = /^(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-])(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
+        return passwordRegex.test(password);
+    };
 
     const handleSignUpClick = () => {
+        setShowPassword(false);
+        resetState();
         const container = document.getElementById('container');
         container.classList.add("right-panel-active");
     }
 
     const handleSignInClick = () => {
+        setShowPassword(false);
+        resetState();
         const container = document.getElementById('container');
         container.classList.remove("right-panel-active");
+    }
+
+    const handleCreateNewAccount = () => {
+        
+        console.log('@ fullname', fullName);
+        console.log('@ email', email);
+        console.log('@ password', password);
+    }
+
+    const handleSignIn = () => {
+        
+        console.log('@ email', email);
+        console.log('@ password', password);
+    }
+
+    const resetState = () => {
+        setFullName('');
+        setEmail('');
+        setPassword('');
+    }
+
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword)
     }
     return (
         <>
@@ -20,59 +65,131 @@ export default function LoginComponent() {
                         <form action="#">
                             <h1>Create Account</h1>
                             <div className="social-container">
-                                <a href="#" className="social">
-                                    <i className="fab fa-facebook-f" />
-                                </a>
-                                <a href="#" className="social">
-                                    <i className="fab fa-google-plus-g" />
-                                </a>
-                                <a href="#" className="social">
-                                    <i className="fab fa-linkedin-in" />
-                                </a>
+                                <i className="social fab fa-facebook-f" />
+                                <i className="social fab fa-google-plus-g" />
+                                <i className="social fab fa-linkedin-in" />
                             </div>
                             <span>or use your email for registration</span>
-                            <input type="text" placeholder="Name" />
-                            <input type="email" placeholder="Email" />
-                            <input type="password" placeholder="Password" />
-                            <button>Sign Up</button>
+                            <TextField
+                                size="small"
+                                type='text'
+                                className='input'
+                                id="outlined-basic-name"
+                                label="Full Name"
+                                variant="outlined"
+                                onChange={(e) => setFullName(e.target.value)}
+                            />
+                            <TextField
+                                size="small"
+                                type='email'
+                                className='input'
+                                id="outlined-basic-email"
+                                label="Email"
+                                variant="outlined"
+                                onChange={(e) => {
+                                    setEmail(e.target.value);
+                                    setIsEmailValid(validateEmail(e.target.value));
+                                }}
+                            />
+                            <TextField
+                                size="small"
+                                className='input'
+                                type={showPassword ? 'text' : 'password'}
+                                id="outlined-basic-password"
+                                label="Password"
+                                variant="outlined"
+                                onChange={(e) => {
+                                    setPassword(e.target.value);
+                                    setIsPasswordValid(validatePassword(e.target.value));
+                                }}
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                edge="end"
+                                            >
+                                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    ),
+                                }} />
+                            <Button
+                                type='button'
+                                className='button'
+                                variant="contained"
+                                disabled={!isEmailValid || !isPasswordValid}
+                                onClick={handleCreateNewAccount}
+                            >
+                                Sign Up</Button>
                         </form>
                     </div>
                     <div className="form-container sign-in-container">
                         <form action="#">
                             <h1>Sign in</h1>
                             <div className="social-container">
-                                <a href="#" className="social">
-                                    <i className="fab fa-facebook-f" />
-                                </a>
-                                <a href="#" className="social">
-                                    <i className="fab fa-google-plus-g" />
-                                </a>
-                                <a href="#" className="social">
-                                    <i className="fab fa-linkedin-in" />
-                                </a>
+                                <i className="social fab fa-facebook-f" />
+                                <i className="social fab fa-google-plus-g" />
+                                <i className="social fab fa-linkedin-in" />
                             </div>
                             <span>or use your account</span>
-                            <input type="email" placeholder="Email" />
-                            <input type="password" placeholder="Password" />
+                            <TextField
+                                size="small"
+                                type='email'
+                                className='input'
+                                id="outlined-basic-p-email"
+                                label="Email"
+                                variant="outlined"
+                                onChange={(e) => {
+                                    setEmail(e.target.value);
+                                    setIsEmailValid(validateEmail(e.target.value));
+                                }}
+                            />
+                            <TextField
+                                size="small"
+                                className='input'
+                                type={showPassword ? 'text' : 'password'}
+                                id="outlined-basic-p-password"
+                                label="Password"
+                                variant="outlined"
+                                onChange={(e) => {
+                                    setPassword(e.target.value);
+                                    setIsPasswordValid(validatePassword(e.target.value));
+                                }}
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                edge="end"
+                                            >
+                                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    ),
+                                }} />
                             <a href="#">Forgot your password?</a>
-                            <button>Sign In</button>
+                            <Button
+                                type='button'
+                                className='button'
+                                variant="contained"
+                                onClick={handleSignIn}
+                            >Sign In</Button>
                         </form>
                     </div>
                     <div className="overlay-container">
                         <div className="overlay">
                             <div className="overlay-panel overlay-left">
                                 <h1>Welcome Back!</h1>
-                                <p>To keep connected with us please login with your personal info</p>
-                                <button onClick={handleSignInClick} className="ghost" id="signIn">
-                                    Sign In
-                                </button>
+                                <p>To keep connected with us please login with your personal info.</p>
+                                <Button type='button' className='ghost' variant="contained" onClick={handleSignInClick}>Sign In</Button>
                             </div>
                             <div className="overlay-panel overlay-right">
                                 <h1>Hello, Friend!</h1>
-                                <p>Enter your personal details and start journey with us</p>
-                                <button className="ghost" id="signUp" onClick={handleSignUpClick}>
-                                    Sign Up
-                                </button>
+                                <p>Create new account and start journey with us.</p>
+                                <Button type='button' className='ghost' variant="contained" onClick={handleSignUpClick}>Sign Up</Button>
                             </div>
                         </div>
                     </div>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// import 'typeface-montserrat';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
